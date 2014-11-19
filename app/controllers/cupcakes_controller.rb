1b572d6d07a833ef04a98c07df6c23b60b9481c2
roryg11 <Rory.C.Grant@gmail.com>
2014-11-19 13:20:17 -0800

    finished? cupcake exercise files

--- app/controllers/cupcakes_controller.rb
+++ app/controllers/cupcakes_controller.rb
@@ -1,6 +1,6 @@
 class CupcakesController < ApplicationController
   def vanilla
-    @toppings = ['sprinkles', 'frosting', 'gummy worms']
+    @toppings = ['sprinkles', 'frosting', 'gummy worms',]
   end
 
   def chocolate
@@ -12,10 +12,21 @@ class CupcakesController < ApplicationController
   end
 
   def maple_bacon
+    @sizes = ['bite', 'regular','instead of dinner', 'for the munchies',]
 
   end
 
   def red_velvet
+    @ingredients = {
+      "all purpose flour" => "3 cups",
+      "granulated sugar" => "3/4 cup",
+      "butter" => "4 tbsp",
+      "unsweetened cocoa" => "2.5 tbsp",
+      "buttermilk" => ".5 cups",
+      "white wine vinegar" => "1.5 tsp",
+      "vanilla extract" => ".5 tsp",
+      "Martha Stewart" => "1",
+    }
 
   end
 end
